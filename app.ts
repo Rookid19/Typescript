bb2bfc23c243ad20bc094f6c4604792e48f311ae
rookid19 <>
2023-03-15 13:46:19 +0000

    literal type

--- app.ts
+++ app.ts
@@ -6,24 +6,23 @@ enum Role {
 }
 
 enum Color {
-    Red,
-    Green,
-    Blue,
-  }
-  
-  /**
-   *
-   *We can use an enum in TypeScript to define a variable or function parameter with a specific set of allowed values. For example, we could define a function that only accepts values from the Color enum
-   * @param {Color} color
-   */
-  function printColor(color: Color) {
-    console.log(color);
-  }
-  
-  printColor(Color.Red); // logs 0
-  printColor(Color.Green); // logs 1
-  printColor(Color.Blue); // logs 2
-  
+  Red,
+  Green,
+  Blue,
+}
+
+/**
+ *
+ *We can use an enum in TypeScript to define a variable or function parameter with a specific set of allowed values. For example, we could define a function that only accepts values from the Color enum
+ * @param {Color} color
+ */
+function printColor(color: Color) {
+  console.log(color);
+}
+
+printColor(Color.Red); // logs 0
+printColor(Color.Green); // logs 1
+printColor(Color.Blue); // logs 2
 
 const person = {
   name: "Maximilian",
@@ -32,13 +31,20 @@ const person = {
   role: Role.ADMIN,
 };
 
-//leteral types
+/**
+ *
+ *
+ * @param {(number | string)} input // union type
+ * @param {(number | string)} input2
+ * Literal types allow you to specify that a variable or parameter must have a specific or exact, literal value, rather than just any value of a certain type
+ * Example "as-number" | "as-text"
+ * @param {("as-number" | "as-text")} resultConversion
+ */
 function combine(
-  input: number | string,
+  input: number | string, 
   input2: number | string,
-  resultConversion: "as-number" | "as-text" //leteral types
+  resultConversion: "as-number" | "as-text" //literal types
 ) {
   if ((resultConversion = "as-number")) {
   }
 }
-
