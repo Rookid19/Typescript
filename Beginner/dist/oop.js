bf498259c63a1defae7586b1a8b38c8fc62bbe81
rookid19 <>
2023-03-29 15:17:11 +0000

    private and public ass modifiers

--- Beginner/dist/oop.js
+++ Beginner/dist/oop.js
@@ -3,13 +3,27 @@
 // setters => change (mutate) them
 var Department = /** @class */ (function () {
     function Department(n) {
+        this.employees = [];
         this.name = n;
     }
+    // this keyword is to refer to class property or a method inside a class
     Department.prototype.describe = function () {
-        console.log('Department ' + this.name);
+        console.log("Department " + this.name);
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees);
     };
     return Department;
 }());
-// to refer to class property or a method inside we use the this keyword
 var accounting = new Department("Accounting");
+accounting.addEmployee("Ralph");
+accounting.addEmployee("Josh");
+// accounting.employees[2] = 'ama'// this works when employees array is public
+// we don't want to this because we want to maintain one method and approach
 accounting.describe();
+accounting.printEmployeeInformation();
+var accountingCopy = { name: "fred", describe: accounting.describe };
+// accountingCopy.describe();
--- Beginner/src/oop.ts
+++ Beginner/src/oop.ts
@@ -3,22 +3,37 @@
 
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
   }
 
-  // this keyword to refer to class property or a method inside we use the this keyword
+  // this keyword is to refer to class property or a method inside a class
 
-  describe() {
+  describe(this: Department) {
     console.log("Department " + this.name);
+  }
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees); 
   }
 }
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployee("Ralph");
+accounting.addEmployee("Josh");
+
+// accounting.employees[2] = 'ama'// this works when employees array is public
+// we don't want to this because we want to maintain one method and approach
+
 accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = { describe: accounting.describe };
+const accountingCopy = { name: "fred", describe: accounting.describe };
 
-accountingCopy.describe();
+// accountingCopy.describe();
