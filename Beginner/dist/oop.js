ee3acfe18727321eeac572d082a4ffddd6b3c7d3
rookid19 <>
2023-04-03 15:28:43 +0000

    inheritance

--- Beginner/dist/oop.js
+++ Beginner/dist/oop.js
@@ -1,6 +1,21 @@
 "use strict";
 // getters => access properties
 // setters => change (mutate) them
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 /**
  *
  *
@@ -31,12 +46,49 @@ var Department = /** @class */ (function () {
     };
     return Department;
 }());
-var accounting = new Department('1', "Accounting");
-accounting.addEmployee("Ralph");
-accounting.addEmployee("Josh");
+/**
+ * Inheritance ---> We can inherit class Department using extends keyword
+ * we can inherit all the properties and methods of Department class
+ * @super calls the constructor of the base class
+ * @class ITDepartment
+ * @extends {Department}
+ */
+var ITDepartment = /** @class */ (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = 
+        //  always remember to call super() in the constructor of the child class
+        _super.call(this, id, 'IT') || this;
+        _this.admins = admins;
+        return _this;
+    }
+    return ITDepartment;
+}(Department));
+var AccountingDepartment = /** @class */ (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(id, reports) {
+        var _this = _super.call(this, id, 'Accounting') || this;
+        _this.reports = reports;
+        return _this;
+    }
+    AccountingDepartment.prototype.addReport = function (text) {
+        this.reports.push(text);
+    };
+    AccountingDepartment.prototype.printReports = function () {
+        console.log(this.reports);
+    };
+    return AccountingDepartment;
+}(Department));
+var IT = new ITDepartment('1', ['randy']);
+IT.addEmployee("Ralph");
+IT.addEmployee("Josh");
 // accounting.employees[2] = 'ama'// this works when employees array is public
 // we don't want to this because we want to maintain one method and approach
-accounting.describe();
-accounting.printEmployeeInformation();
-var accountingCopy = { name: "fred", describe: accounting.describe };
+IT.describe();
+IT.printEmployeeInformation();
+console.log(IT);
+var accounting = new AccountingDepartment('2', []);
+accounting.addReport("Something went wrong.....");
+accounting.printReports();
+// const accountingCopy = { name: "fred", describe: accounting.describe };
 // accountingCopy.describe();
--- Beginner/src/oop.ts
+++ Beginner/src/oop.ts
@@ -33,17 +33,58 @@ constructor(private readonly id:string, public name: string){}
   }
 }
 
-const accounting = new Department('1',"Accounting");
+/**
+ * Inheritance ---> We can inherit class Department using extends keyword
+ * we can inherit all the properties and methods of Department class
+ * @super calls the constructor of the base class
+ * @class ITDepartment
+ * @extends {Department}
+ */
+class ITDepartment extends Department{
+  constructor(id:string, public admins: string[]){
+  //  always remember to call super() in the constructor of the child class
+    super(id,'IT');
+  }
+}
 
-accounting.addEmployee("Ralph");
-accounting.addEmployee("Josh");
+/**
+ *
+ *
+ * @class AccountingDepartment
+ * @extends {Department}
+ */
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id,'Accounting')
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const IT = new ITDepartment('1',['randy']);
+
+IT.addEmployee("Ralph");
+IT.addEmployee("Josh");
 
 // accounting.employees[2] = 'ama'// this works when employees array is public
 // we don't want to this because we want to maintain one method and approach
 
-accounting.describe();
-accounting.printEmployeeInformation();
+IT.describe();
+IT.printEmployeeInformation();
 
-const accountingCopy = { name: "fred", describe: accounting.describe };
+console.log(IT)
+
+const accounting = new AccountingDepartment('2',[]);
+
+accounting.addReport("Something went wrong.....");
+accounting.printReports();
+// const accountingCopy = { name: "fred", describe: accounting.describe };
 
 // accountingCopy.describe();
+
