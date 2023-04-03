8d877d46f3d510585b2c18627174b6c4bc69c744
rookid19 <>
2023-04-03 16:35:59 +0000

    getters and setters

--- Beginner/dist/oop.js
+++ Beginner/dist/oop.js
@@ -32,11 +32,14 @@ var Department = /** @class */ (function () {
         this.name = name;
         //private readonly id : string
         // name: string;
+        // private properties can only be accessed inside it class and not outside
+        //protected properties can be accessed inside it class and its child class(in other words any class that extends the base class)
+        // private employees: string[] = [];
         this.employees = [];
     }
     // this keyword is to refer to class property or a method inside a class
     Department.prototype.describe = function () {
-        console.log("Department " + (this.id) + " " + this.name);
+        console.log("Department " + this.id + " " + this.name);
     };
     Department.prototype.addEmployee = function (employee) {
         this.employees.push(employee);
@@ -58,28 +61,62 @@ var ITDepartment = /** @class */ (function (_super) {
     function ITDepartment(id, admins) {
         var _this = 
         //  always remember to call super() in the constructor of the child class
-        _super.call(this, id, 'IT') || this;
+        _super.call(this, id, "IT") || this;
         _this.admins = admins;
         return _this;
     }
     return ITDepartment;
 }(Department));
+/**
+ *
+ *
+ * @class AccountingDepartment
+ * @extends {Department}
+ */
 var AccountingDepartment = /** @class */ (function (_super) {
     __extends(AccountingDepartment, _super);
-    function AccountingDepartment(id, reports) {
-        var _this = _super.call(this, id, 'Accounting') || this;
+    function AccountingDepartment(id, reports, lastReport) {
+        if (lastReport === void 0) { lastReport = reports[0]; }
+        var _this = _super.call(this, id, "Accounting") || this;
         _this.reports = reports;
+        _this.lastReport = lastReport;
         return _this;
+        // this.lastReport = reports[0];
     }
+    Object.defineProperty(AccountingDepartment.prototype, "mostRecentReport", {
+        // private lastReport: string;
+        // we can use getter and setter to access private properties
+        get: function () {
+            if (this.lastReport) {
+                return this.lastReport;
+            }
+            throw new Error("No report found");
+        },
+        set: function (value) {
+            if (!value) {
+                throw new Error("Please pass in a valid value");
+            }
+            this.addReport(value);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    AccountingDepartment.prototype.addEmployee = function (employee) {
+        if (employee === "Ralph") {
+            return;
+        }
+        this.employees.push(employee);
+    };
     AccountingDepartment.prototype.addReport = function (text) {
         this.reports.push(text);
+        this.lastReport = text;
     };
     AccountingDepartment.prototype.printReports = function () {
         console.log(this.reports);
     };
     return AccountingDepartment;
 }(Department));
-var IT = new ITDepartment('1', ['randy']);
+var IT = new ITDepartment("1", ["randy"]);
 IT.addEmployee("Ralph");
 IT.addEmployee("Josh");
 // accounting.employees[2] = 'ama'// this works when employees array is public
@@ -87,8 +124,12 @@ IT.addEmployee("Josh");
 IT.describe();
 IT.printEmployeeInformation();
 console.log(IT);
-var accounting = new AccountingDepartment('2', []);
+var accounting = new AccountingDepartment("2", []);
+accounting.mostRecentReport = "My name is barry allen";
 accounting.addReport("Something went wrong.....");
+console.log(accounting.mostRecentReport);
+accounting.addEmployee("Max");
 accounting.printReports();
+accounting.printEmployeeInformation();
 // const accountingCopy = { name: "fred", describe: accounting.describe };
 // accountingCopy.describe();
--- Beginner/src/oop.ts
+++ Beginner/src/oop.ts
@@ -2,14 +2,18 @@
 // setters => change (mutate) them
 
 /**
- * 
+ *
  *
  * @class Department
  */
 class Department {
   //private readonly id : string
   // name: string;
-  private employees: string[] = [];
+
+  // private properties can only be accessed inside it class and not outside
+  //protected properties can be accessed inside it class and its child class(in other words any class that extends the base class)
+  // private employees: string[] = [];
+  protected employees: string[] = [];
 
   // constructor(n: string) {
   //   this.name = n;
@@ -17,19 +21,19 @@ class Department {
 
   // shorthand initialization
   // readonly is used to initialize property once
-constructor(private readonly id:string, public name: string){}
+  constructor(private readonly id: string, public name: string) {}
 
   // this keyword is to refer to class property or a method inside a class
 
   describe(this: Department) {
-    console.log("Department " +(this.id)+" " + this.name);
+    console.log("Department " + this.id + " " + this.name);
   }
   addEmployee(employee: string) {
     this.employees.push(employee);
   }
 
   printEmployeeInformation() {
-    console.log(this.employees); 
+    console.log(this.employees);
   }
 }
 
@@ -40,10 +44,10 @@ constructor(private readonly id:string, public name: string){}
  * @class ITDepartment
  * @extends {Department}
  */
-class ITDepartment extends Department{
-  constructor(id:string, public admins: string[]){
-  //  always remember to call super() in the constructor of the child class
-    super(id,'IT');
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    //  always remember to call super() in the constructor of the child class
+    super(id, "IT");
   }
 }
 
@@ -54,12 +58,42 @@ class ITDepartment extends Department{
  * @extends {Department}
  */
 class AccountingDepartment extends Department {
-  constructor(id: string, private reports: string[]) {
-    super(id,'Accounting')
+  // private lastReport: string;
+
+  // we can use getter and setter to access private properties
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value");
+    }
+    this.addReport(value);
+  }
+
+  constructor(
+    id: string,
+    private reports: string[],
+    private lastReport: string = reports[0]
+  ) {
+    super(id, "Accounting");
+    // this.lastReport = reports[0];
+  }
+
+  addEmployee(employee: string): void {
+    if (employee === "Ralph") {
+      return;
+    }
+    this.employees.push(employee);
   }
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport= text; 
   }
 
   printReports() {
@@ -67,7 +101,7 @@ class AccountingDepartment extends Department {
   }
 }
 
-const IT = new ITDepartment('1',['randy']);
+const IT = new ITDepartment("1", ["randy"]);
 
 IT.addEmployee("Ralph");
 IT.addEmployee("Josh");
@@ -78,13 +112,20 @@ IT.addEmployee("Josh");
 IT.describe();
 IT.printEmployeeInformation();
 
-console.log(IT)
+console.log(IT);
 
-const accounting = new AccountingDepartment('2',[]);
+const accounting = new AccountingDepartment("2", []);
 
+accounting.mostRecentReport = "My name is barry allen";
 accounting.addReport("Something went wrong.....");
+
+console.log(accounting.mostRecentReport)
+
+
+accounting.addEmployee("Max");
 accounting.printReports();
+
+accounting.printEmployeeInformation();
 // const accountingCopy = { name: "fred", describe: accounting.describe };
 
 // accountingCopy.describe();
-
